56df2f337cbabcbc2bc4602ffacec04023dcd53e
claytonreis33 <133873614+claytonreis33@users.noreply.github.com>
2024-06-03 10:32:11 -0300

    Second Commit
    Alterado o arquivo Main.kt ,foi implementado a variavel name e incorporado a função main.

--- Main.kt
+++ Main.kt
@@ -1,3 +1,8 @@
+// val -> delacração de variável somente leitura
+//var -> delacração de variável mutavel
+
+val name ="Davi"
+
 fun main(){
-  println("Hello, world!")
+  println("Hello, $name!")
 }
